5831f417b6acee210cb6e46fbfa8cf1960bce49d
LachlanCourt <lachlan.m.court@gmail.com>
2021-08-07 16:53:51 +1000

    Implement ProcessEvents

--- src/Process.java
+++ src/Process.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Process
 {
     private String id;
@@ -5,12 +7,26 @@ public class Process
     private int execSize;
     private int tickets;
 
+    private ArrayList<ProcessEvent> processHistory;
+
     public Process(String id_, int arrive_, int execSize_, int tickets_)
     {
         id = id_;
         arrive = arrive_;
         execSize = execSize_;
         tickets = tickets_;
+
+        processHistory = new ArrayList<>();
+    }
+
+    public void addEvent(ProcessEvent event)
+    {
+        processHistory.add(event);
+    }
+
+    public ArrayList<ProcessEvent> getEvents()
+    {
+        return processHistory;
     }
 
     public String getId() {
