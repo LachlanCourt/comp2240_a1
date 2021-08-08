8fc3709d27e05237dd47fc6d035c1ffaebbd0409
LachlanCourt <lachlan.m.court@gmail.com>
2021-08-08 11:31:56 +1000

    Implement algorithm less getNextProcess method

--- src/LTR.java
+++ src/LTR.java
@@ -1,32 +1,46 @@
-public class LTR extends Algorithm {
+import java.util.Random;
+public class LTR extends Algorithm
+{
 
     public LTR()
     {
         name = "LTR";
     }
 
-    @Override
-    protected int getNextProcess() {
+    @Override protected int getNextProcess()
+    {
+        Random rand = new Random(844422);
+        System.out.println(rand.nextInt(550));
+
         return 0;
     }
 
-    @Override
-    protected void run() {
+    @Override protected void run()
+    {
         currentTime = 0;
         Process currentProcess;
         addNewProcesses();
         while (unfinishedProcesses.size() > 0)
         {
             currentTime += DISP;
-            currentProcess = unfinishedProcesses.get(getNextProcess());
-            ProcessEvent event =
-                    new ProcessEvent(currentTime, currentTime + currentProcess.getRemainingTime(), currentProcess.getId());
+            int nextProcessIndex = getNextProcess();
+            currentProcess = unfinishedProcesses.get(nextProcessIndex);
+            int startTime = currentTime;
+            while (currentTime - startTime < 4)
+            {
+                currentTime++;
+                currentProcess.decRemainingTime();
+                addNewProcesses();
+                if (currentProcess.getRemainingTime() == 0)
+                {
+                    unfinishedProcesses.remove(currentProcess);
+                    finishedProcesses.add(currentProcess);
+                    break;
+                }
+            }
+            ProcessEvent event = new ProcessEvent(startTime, currentTime, currentProcess.getId());
             currentProcess.addEvent(event);
-            currentTime += currentProcess.getRemainingTime();
-            unfinishedProcesses.remove(currentProcess);
-            finishedProcesses.add(currentProcess);
             processEventRecord.add(event);
-            addNewProcesses();
         }
     }
 }
